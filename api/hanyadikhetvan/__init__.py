1057a92944e3aaf119712de498192372e39bba6f
Kristóf Torma <tormakristof@tormakristof.eu>
2022-07-31 19:42:55 +0200

    remove debugging bullshit

--- api/hanyadikhetvan/__init__.py
+++ api/hanyadikhetvan/__init__.py
@@ -7,7 +7,6 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     startdate = date(2022, 8, 29)
     currentweek = floor((date.today() - startdate).days / 7)
-    request.get("https://tormakristof.eu/")
     return func.HttpResponse(
         str(currentweek),
         status_code=200)
