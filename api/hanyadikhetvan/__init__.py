67b139fbeed18bfe27b027e71fcae653e71c19e2
Kristóf Torma <tormakristof@tormakristof.eu>
2022-07-31 19:32:13 +0200

    debug bullshit

--- api/hanyadikhetvan/__init__.py
+++ api/hanyadikhetvan/__init__.py
@@ -1,11 +1,13 @@
 from math import floor
 from datetime import date
+from urllib import request
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     startdate = date(2022, 8, 29)
     currentweek = floor((date.today() - startdate).days / 7)
+    request.get("https://tormakristof.eu/")
     return func.HttpResponse(
         str(currentweek),
         status_code=200)
