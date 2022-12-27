3e7cb0f18ae1aef9480c5ad009cc816ce62fc384
Torma Kristóf <tormakristof@tormakristof.eu>
2022-12-27 13:42:24 +0100

    update with new start of term

--- api/hanyadikhetvan/__init__.py
+++ api/hanyadikhetvan/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    startdate = date(2022, 8, 29)
+    startdate = date(2023, 2, 27)
     currentweek = floor((date.today() - startdate).days / 7)
     return func.HttpResponse(
         str(currentweek),
