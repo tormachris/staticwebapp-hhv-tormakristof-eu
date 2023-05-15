c915a5feeabc603e25f5188285ceb5e7fb04e8ea
Torma Kristóf <tormakristof@tormakristof.eu>
2023-05-15 11:35:16 +0200

    update week start

--- api/hanyadikhetvan/__init__.py
+++ api/hanyadikhetvan/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    startdate = date(2023, 2, 27)
+    startdate = date(2023, 2, 20)
     currentweek = floor((date.today() - startdate).days / 7)
     return func.HttpResponse(
         str(currentweek),
